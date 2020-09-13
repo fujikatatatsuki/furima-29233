818df2bda2ce068aabb829fd7d76366243362007
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-13 17:58:34 +0900

    with_optionsを使用して、presence:trueをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # - ニックネームが必須であること
-  validates :nickname, presence: true
+  with_options presence: true do
 
+    # - ニックネームが必須であること
+    validates :nickname
+
+    # - ユーザー本名が、名字と名前がそれぞれ必須であること
+    # - ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること
+    VALID_NAME_REGEX = /[ぁ-んァ-ン一-龥]/.freeze
+    validates :full_width_family_name, format: { with: VALID_NAME_REGEX }
+
+    validates :full_width_name, format: {with: VALID_NAME_REGEX }
+
+    # - ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること
+    # - ユーザー本名のフリガナは全角（カタカナ）で入力させること
+    VALID_KANA_NAME_REGEX = /[ァ-ン]/.freeze
+    validates :full_width_kana_family_name, format: {with: VALID_KANA_NAME_REGEX }
+    validates :full_width_kana_name, format: {with: VALID_KANA_NAME_REGEX }
+
+    # - 生年月日が必須であること
+    validates :birthday
+  end
   # - メールアドレスが必須であること デフォルト
   # - メールアドレスが一意性であること デフォルト
   # - メールアドレスは@を含む必要があること デフォルト
@@ -17,20 +35,4 @@ class User < ApplicationRecord
   # - パスワードは確認用を含めて2回入力すること デフォルト
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-
-  # - ユーザー本名が、名字と名前がそれぞれ必須であること
-  # - ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること
-  VALID_NAME_REGEX = /[ぁ-んァ-ン一-龥]/.freeze
-  validates :full_width_family_name, presence: true, format: { with: VALID_NAME_REGEX }
-
-  validates :full_width_name, presence: true, format: {with: VALID_NAME_REGEX }
-
-  # - ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること
-  # - ユーザー本名のフリガナは全角（カタカナ）で入力させること
-  VALID_KANA_NAME_REGEX = /[ァ-ン]/.freeze
-  validates :full_width_kana_family_name, presence: true, format: {with: VALID_KANA_NAME_REGEX }
-  validates :full_width_kana_name, presence: true, format: {with: VALID_KANA_NAME_REGEX }
-
-  # - 生年月日が必須であること
-  validates :birthday, presence: true
 end
