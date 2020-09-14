bcc1d2b486450c64a068a0b881971e680daf4211
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-14 12:48:42 +0900

    usersモデルと、itemsテーブルのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
 
+  # - 共通で入力必須のバリテーションを設定
   with_options presence: true do
 
     # - ニックネームが必須であること
