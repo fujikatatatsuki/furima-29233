f2336463a0b08c53315df4784cf82fb54a39a936
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-14 15:22:43 +0900

    ActiveHashのアソシエーション・バリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,28 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belong_to_active_hash :category
+  belong_to_active_hash :status
+  belong_to_active_hash :delivery_fee
+  belong_to_active_hash :shipping_region
+  belong_to_active_hash :shipping_day
+
+  # 共通で、空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :category
+    validates :status
+    validates :delivery_fee
+    validates :shipping_region
+    validates :shipping_day
+  end
+
+  # 共通で、選択が「---」の時は保存できないようにする
+  with_options numericality: { other_than: 1 }  do
+    validates :category
+    validates :status
+    validates :delivery_fee
+    validates :shipping_region
+    validates :shipping_day
+  end
 end
