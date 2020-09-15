7317cc34e42ef7de24e92a43c0552ae6716e7fbb
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-15 20:31:02 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,19 +11,27 @@ class Item < ApplicationRecord
 
   # 共通で、空の投稿を保存できないようにする
   with_options presence: true do
-    validates :category
-    validates :status
-    validates :delivery_fee
-    validates :shipping_region
-    validates :shipping_day
+    validates :image
+    validates :name
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :shipping_region_id
+    validates :shipping_day_id
   end
 
   # 共通で、選択が「---」の時は保存できないようにする
   with_options numericality: { other_than: 1 }  do
-    validates :category
-    validates :status
-    validates :delivery_fee
-    validates :shipping_region
-    validates :shipping_day
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :shipping_region_id
+    validates :shipping_day_id
   end
+  # 販売価格の範囲が、¥300~¥9,999,999の間であること
+  validates :delivery_fee_id, numericality => { :greater_than_or_equal_to => 300 && less_than_or_equal_to => 9999999 }
+
+  # 販売価格は半角数字のみ入力可能
+  DELIVERY_REGEX = /[0-9\d]/
+  validates :delivery_fee_id, format: [with: DELIVERY_REGEX ]
 end
