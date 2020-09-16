fbf47688659ad4f5c5dbf4f3625bcc510c05d648
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-16 11:57:57 +0900

    商品出品機能のエラー画面が出ることを確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :image
     validates :name
-    validates :category_id
-    validates :status_id
-    validates :delivery_fee_id
-    validates :shipping_region_id
-    validates :shipping_day_id
+    # validates :category_id
+    # validates :status_id
+    # validates :delivery_fee_id
+    # validates :shipping_region_id
+    # validates :shipping_day_id
   end
 
   # 共通で、選択が「---」の時は保存できないようにする
@@ -28,10 +28,10 @@ class Item < ApplicationRecord
     validates :shipping_region_id
     validates :shipping_day_id
   end
-  # 販売価格の範囲が、¥300~¥9,999,999の間であること
-  validates :delivery_fee_id, numericality => { :greater_than_or_equal_to => 300 && less_than_or_equal_to => 9999999 }
+  # 販売価格の範囲が、¥300以上~¥9,999,999未満であること
+  validates :delivery_fee_id, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   # 販売価格は半角数字のみ入力可能
-  DELIVERY_REGEX = /[0-9\d]/
-  validates :delivery_fee_id, format: [with: DELIVERY_REGEX ]
+  VALID_DELIVERY_FEE_REGEX = /[0-9\d]/
+  validates :delivery_fee_id, format: { with: VALID_DELIVERY_FEE_REGEX }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    # image              {  }
+    # name               {  }
+    # explanation        {  }
+    # category_id        {  }
+    # status_id          {  }
+    # delivery_fee_id    {  }
+    # shipping_region_id {  }
+    # shipping_day_id    {  }
+    # selling_price      {  }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
