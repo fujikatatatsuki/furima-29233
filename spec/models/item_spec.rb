95fa8e989b3dbabcb5bca2c215245f2a8269933c
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-16 17:52:18 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,47 +8,6 @@ describe Item do
   describe '商品出品' do
     context '商品出品が成功する場合' do
       it '全ての値が正しく入力されていれば登録できる' do
-        expect(@user).to be_valid
-        expect(@item).to be_valid
-      end
-      it '画像(image)が登録されていれば登録できる' do
-        @item.image
-        expect(@item).to be_valid
-      end
-      it '商品名(name)が入力されていれば登録できる' do
-        @item.name
-        expect(@item).to be_valid
-      end
-      it 'カテゴリー(category_id)の情報が入力がされていれば登録できる' do
-        @item.category_id
-        expect(@item).to be_valid
-      end
-      it '商品の状態(status_id)についての情報が入力されていれば登録できる' do
-        @item.status_id
-        expect(@item).to be_valid
-      end
-      it '配送料(delivery_fee_id)の負担についての情報が入力されていれば登録できる' do
-        @item.delivery_fee_id
-        expect(@item).to be_valid
-      end
-      it '発送元の地域(shipping_region_id)についての情報が入力されていれば登録できる' do
-        @item.shipping_region_id
-        expect(@item).to be_valid
-      end
-      it '発送までの日数(shipping_day_id)についての情報が入力されていれば登録できる' do
-        @item.shipping_day_id
-        expect(@item).to be_valid
-      end
-      it '販売価格(selling_price)についての情報が入力されていれば登録できる' do
-        @item.selling_price
-        expect(@item).to be_valid
-      end
-      it '販売価格(selling_price)の範囲が、¥300~¥9,999,999の間であれば登録できる' do
-        @item.selling_price
-        expect(@item).to be_valid
-      end
-      it '販売価格(selling_price)は半角数字のみで入力されていれば登録できる' do
-        @item.selling_price
         expect(@item).to be_valid
       end
     end
