881b6b2720cfcd493aa966fdbb4a48aabc3d6d25
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-19 16:27:31 +0900

    editアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order('created_at ASC')
@@ -20,6 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
   end
 
   private
