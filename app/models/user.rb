81e73e44aba310dd78af108feb19c3802521c047
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-21 09:25:49 +0900

    アソシエーションの再定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
+  has_many :orders
 
   # - 共通で入力必須のバリテーションを設定
   with_options presence: true do
