bc5cca6451f5ce0072093d56712db65269774932
fujikatatatsuki <t1t2.40001@gmail.com>
2020-09-23 16:49:42 +0900

    フォームオブジェクトの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,34 @@
 class OrdersController < ApplicationController
   def index
-    # @order = Order.new
-    # @item = Item.find(params[:item_id])
+    @order = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
+
+  private
+
+  def order_params
+    params.permit(:shipping_region_id, :postal_code, :city, :block_number, :building_name, :phone_number, :token, :item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_b09b3667b0acc0b0e1521ef2"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.selling_price,   # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+
 end
